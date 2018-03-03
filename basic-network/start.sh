9c57ee174684f4322b245fb73aadc8bc2dfffe86
Matt Johnson <matt9j@cs.washington.edu>
2018-03-02 18:33:59 -0800

    [ledger] Switch basic-network to goleveldb
    CouchDB is overkill for the design, and adds significant system
    complexity

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start all required containers, recreating underlying state.
-docker-compose -f docker-compose.yml up -d --renew-anon-volumes ca.example.com orderer.example.com peer0.org1.example.com couchdb
+docker-compose -f docker-compose.yml up -d --renew-anon-volumes ca.example.com orderer.example.com peer0.org1.example.com
 
 # Create the channel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
