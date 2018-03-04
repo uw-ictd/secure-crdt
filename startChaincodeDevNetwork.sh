e8a9c15e3d623ea807197d6ca8dd17c54394a021
Matt Johnson <matt9j@cs.washington.edu>
2018-03-03 16:41:53 -0800

    [ledger] Use admin credentials for creating channels

--- startChaincodeDevNetwork.sh
+++ startChaincodeDevNetwork.sh
@@ -10,9 +10,9 @@ pushd ./basic-network/
 docker-compose -f docker-compose-dev.yml -f docker-compose.yml up -d --renew-anon-volumes ca.example.com orderer.example.com peer0.org1.example.com.dev chaincode
 
 # Create the channel
-docker exec peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
-docker exec peer0.org1.example.com peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
 
 # Launch the CLI container to install and instantiate the chaincode on the peer.
 docker-compose -f ./docker-compose.yml up -d cli
