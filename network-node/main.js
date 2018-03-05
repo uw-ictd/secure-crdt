7ac171300d2573c43a77a5ccdb725e626a5e2287
Matt Johnson <matt9j@cs.washington.edu>
2018-03-05 02:03:06 -0800

    [node] Basic REST account api

--- network-node/main.js
+++ network-node/main.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 const upload = require('multer')();
 const Fabric_Client = require('fabric-client');
 const path = require('path');
+const HashSet = require('hashset');
 const cc_executor = require('./execute-chaincode.js');
 
 // Setup the fabric network.
@@ -26,19 +27,56 @@ console.log('Store path:'+store_path);
 // Initialize keys for the cc_executor.
 cc_executor.setupCrypto(fabric_client, store_path);
 
-const port = 8080;
+// Create a store to queue disconnected tuples.
+const backlog = new HashSet();
+let disconnected = false;
 
+// HTTP Server properties.
+const port = 8080;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing xhtml
 
-app.get('/balance/', (request, response) => {
-    console.log(request.body);
-    cc_executor.invokeChaincode(fabric_client, channel, 'account', 'register', ['allan', 'allankeys']);
-    response.send('TODO send the actual response');
+app.get('/disconnected', (request, response) => {
+    response.send({status: disconnected});
 });
 
-app.post('/', upload.array(), (request, response, next) => {
-   console.log(request.body);
+app.post('/disconnected', upload.array(), (request, response, next) => {
+    console.log(request.body);
+    if (request.body.status === 'true') {
+       disconnected = true;
+       response.status(200).send("Emulating disconnection");
+       return;
+    }
+
+    if (request.body.status === 'false') {
+       disconnected = false;
+       response.status(200).send("System now connected");
+       return;
+    }
+
+    response.status(400).send("Accepts only 'true' or 'false'.")
+});
+
+app.get('/:userId/pubkey', (request, response) => {
+    let userId = request.params.userId;
+    cc_executor.queryChaincode(fabric_client, channel, 'account', 'getPublicKey', [userId]).then((result) => {
+        response.status(200).send({userId: userId, pubKey: result.toString()});
+    }).catch((err) => {
+        response.status(500).send(err.toString());
+    });
+});
+
+app.post('/:userId', upload.array(), (request, response, next) => {
+    if (disconnected === true) {
+        response.status(400).send("Cannot register a new user while disconnected");
+    }
+
+    cc_executor.invokeChaincode(fabric_client, channel, 'account', 'register',
+                                [request.params.userId, request.body.pubKey]).then((result) => {
+        response.status(200).send(result);
+    }).catch((err) => {
+        response.status(500).send(err.toString());
+    });
 });
 
 app.listen(port, (err) => {
